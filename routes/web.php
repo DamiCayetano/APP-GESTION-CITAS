3d4447643d06ad82a93db39b4ca82d4d963b66c8
Gianfranco Huillca <gianfrancohj2@gmail.com>
2025-09-08 01:36:05 -0500

    Actualizo rutas web antes del merge

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,5 @@ Route::get('/system', function () {
 });
 
 Route::resource('patients', PatientsController::class);
-<<<<<<< HEAD
 Route::resource('doctors', DoctorsController::class);
 Route::resource('quotes', QuotesController::class);
-=======
-Route::resource('doctors', DoctorsController::class);
->>>>>>> 6bb3d8611ae1c7dde370bca16871ab470e2df396
